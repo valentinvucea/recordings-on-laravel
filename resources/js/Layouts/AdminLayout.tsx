8b439f13b9cf854d1f66ca92d715619ff11b1b8a
Valentin Vucea <valentinvucea@gmail.com>
2024-12-03 12:48:42 -0500

    Last updates before delete

--- resources/js/Layouts/AdminLayout.tsx
+++ resources/js/Layouts/AdminLayout.tsx
@@ -1,7 +1,5 @@
 import { useState, PropsWithChildren, ReactNode } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import { User } from '@/types';
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,10 +1,10 @@
-import AuthenticatedLayout from '@/Layouts/AdminLayout';
+import AdminLayout from '@/Layouts/AdminLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 
 export default function Dashboard({ auth }: PageProps) {
     return (
-        <AuthenticatedLayout
+        <AdminLayout
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Home</h2>}
         >
@@ -59,6 +59,6 @@ export default function Dashboard({ auth }: PageProps) {
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout >
+        </AdminLayout >
     );
 }
